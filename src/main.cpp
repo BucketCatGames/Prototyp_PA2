800f0e420848d4b787544781a70b8e51b520593d
GiPi03 <147610264+GiPi03@users.noreply.github.com>
2024-04-15 19:01:04 +0200

    fertig

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,100 @@
 
 #include "config.h"
 
+struct Player
+{
+	float posX;
+	float posY;
+	float speed;
+	float xVel;
+	float yVel;
+
+	Texture2D texture;
+};
+
+void InitPlayer(Player& player)
+{
+	player.posX = 100;
+	player.posY = 100;
+	player.speed = 5;
+	player.xVel = 0;
+	player.yVel = 0;
+
+	player.texture = LoadTexture("assets/graphics/testimage.png");
+}
+
+void UpdatePlayer(Player& player)
+{
+	player.posX += player.xVel;
+	player.posY += player.yVel;
+}
+
+void PlayerInput(Player& player)
+{
+	if (IsKeyDown(KEY_W))
+	{
+		player.yVel = -player.speed;
+	}
+	else if (IsKeyDown(KEY_S))
+	{
+		player.yVel = player.speed;
+	}
+	else
+	{
+		player.yVel = 0;
+	}
+
+	if (IsKeyDown(KEY_A))
+	{
+		player.xVel = -player.speed;
+	}
+	else if (IsKeyDown(KEY_D))
+	{
+		player.xVel = player.speed;
+	}
+	else
+	{
+		player.xVel = 0;
+	}
+}
+void DrawPlayer(Player& player)
+{
+	DrawTexture(player.texture, player.posX, player.posY, BLACK);
+}
+
 int main() {
-    // Raylib initialization
-    // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
-    InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
-    SetTargetFPS(60);
+	// Raylib initialization
+	// Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
+	InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
+	SetTargetFPS(60);
 
 #ifdef GAME_START_FULLSCREEN
-    ToggleFullscreen();
+	ToggleFullscreen();
 #endif
 
-    // Your own initialization code here
-    // ...
-    // ...
-    Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
+	Player player;
 
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
-    {
-        // Updates that are made by frame are coded here
-        // ...
-        // ...
+	
+	InitPlayer(player);
 
-        BeginDrawing();
-            // You can draw on the screen between BeginDrawing() and EndDrawing()
-            // ...
-            // ...
-            ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
+	// Main game loop
+	while (!WindowShouldClose()) // Detect window close button or ESC key
+	{
+		PlayerInput(player);
+		UpdatePlayer(player);
 
-        EndDrawing();
-    } // Main game loop end
 
-    // De-initialization here
-    // ...
-    // ...
-    UnloadTexture(myTexture);
+		BeginDrawing();
+		ClearBackground(WHITE);
 
-    // Close window and OpenGL context
-    CloseWindow();
+		DrawPlayer(player);
 
-    return EXIT_SUCCESS;
+		EndDrawing();
+	}
+	//UnloadTexture(myTexture);
+
+	// Close window and OpenGL context
+	CloseWindow();
+
+	return EXIT_SUCCESS;
 }
